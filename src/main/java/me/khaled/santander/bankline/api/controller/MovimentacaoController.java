ac92fd8518d65bd04e0d2026a83a7297d6acedf4
Khaled <khalegjr@gmail.com>
2022-05-15 21:46:01 -0300

    criando a seleção de movimentação por id de conta

--- src/main/java/me/khaled/santander/bankline/api/controller/MovimentacaoController.java
+++ src/main/java/me/khaled/santander/bankline/api/controller/MovimentacaoController.java
@@ -1,12 +1,15 @@
 package me.khaled.santander.bankline.api.controller;
 
 import java.util.List;
+
 import me.khaled.santander.bankline.api.dto.NovaMovimentacao;
 import me.khaled.santander.bankline.api.model.Movimentacao;
 import me.khaled.santander.bankline.api.repository.MovimentacaoRepository;
 import me.khaled.santander.bankline.api.service.MovimentacaoService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +28,11 @@ public class MovimentacaoController {
   @GetMapping
   public List<Movimentacao> findAll() {
     return repository.findAll();
+  }
+
+  @GetMapping("/{idConta}")
+  public List<Movimentacao> findAll(@PathVariable("idConta") Integer idConta) {
+    return repository.findByIdConta(idConta);
   }
 
   @PostMapping
--- src/main/java/me/khaled/santander/bankline/api/repository/MovimentacaoRepository.java
+++ src/main/java/me/khaled/santander/bankline/api/repository/MovimentacaoRepository.java
@@ -3,5 +3,10 @@ package me.khaled.santander.bankline.api.repository;
 import me.khaled.santander.bankline.api.model.Movimentacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MovimentacaoRepository
-  extends JpaRepository<Movimentacao, Integer> {}
+    extends JpaRepository<Movimentacao, Integer> {
+    
+  public List<Movimentacao> findByIdConta(Integer idConta);
+  }
